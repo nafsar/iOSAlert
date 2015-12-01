532a119b59cb33fa7f62acff22e1e6c3355c509c
Naser Afsar <n.afsar@gmail.com>
2015-11-30 22:04:12 -0700

    alert

---
+++ PopOut/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  PopOut
+//
+//  Created by Naser Afsar on 11/30/15.
+//  Copyright © 2015 uiux.biz LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func alertOut() {
+        
+        let popOut = UIAlertController(title: "Swift", message: "Alert function", preferredStyle: .Alert)
+        
+        popOut.addAction(UIAlertAction(title: "Exit", style: .Destructive, handler: nil ))
+        
+        self.presentViewController(popOut, animated: false, completion: nil )
+    }
+}
+
